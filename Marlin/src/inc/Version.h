080e72054ed54b9ddb3b5a97348af8c01857e357
Gaurav <gaurav.mahto2@aristocrat.com>
2021-10-21 12:36:10 +0530

    Updated custom version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.0-gmahto"
+  #define SHORT_BUILD_VERSION "2.0-gmahto " __DATE__ " - " __TIME__
 #endif
 
 /**
